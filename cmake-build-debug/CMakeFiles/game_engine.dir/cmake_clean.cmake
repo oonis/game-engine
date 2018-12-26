d501434eafcce43594f08814b64a63e4376da77b
Sam Macaluso <sam@macalu.so>
2018-12-26 11:39:22 -0500

    init commit

---
+++ cmake-build-debug/CMakeFiles/game_engine.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game_engine.dir/main.cpp.obj"
+  "game_engine.pdb"
+  "game_engine.exe"
+  "game_engine.exe.manifest"
+  "game_engine.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/game_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
